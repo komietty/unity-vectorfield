d1613c854e5c05333b2b99da7567605e912ce9f6
komietty <komietty@gmail.com>
2022-11-29 23:57:15 +0900

    scalar poisson problem(wip)

--- Assets/Core/TangentBundle/TangentBundle.cs
+++ Assets/Core/TangentBundle/TangentBundle.cs
@@ -2,16 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Mathematics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using static Unity.Mathematics.math;
 
 namespace ddg {
     public class TangentBundle {
-        protected float[] tangentField;
+        protected float3[] tangentField;
         protected HeGeom geom;
 
-        float3[] InterpolateWhitney(double[] oneForm) {
+        public TangentBundle(Mesh mesh){
+            geom = new HeGeom(mesh);
+        }
+
+        public static double[] GenRandomOneForm(HeGeom geom) {
+            var nv = geom.nVerts;
+            var r = max(2, (int)(nv / 1000f));
+            var rho1 = DenseMatrix.Create(nv, 1, 0);
+            var rho2 = DenseMatrix.Create(nv, 1, 0);
+            for (var i = 0; i < r; i++) {
+                var j1 = (int)(UnityEngine.Random.value * nv);
+                var j2 = (int)(UnityEngine.Random.value * nv);
+                rho1[j1, 0] = UnityEngine.Random.Range(-2500f, 2500f);
+                rho2[j2, 0] = UnityEngine.Random.Range(-2500f, 2500f);
+            }
+            var scalarPotential = ScalerPoissonProblem.SolveOnSurfaceNative(geom, rho1);
+            var vectorPotential = ScalerPoissonProblem.SolveOnSurfaceNative(geom, rho2);
+
+            var field = new Dictionary<Face, float3>();
+            for (var i = 0; i < geom.nFaces; i++) {
+                var f = geom.Faces[i];
+                var v = new float3();
+                var a = geom.Area(f);
+                var (_, n) = geom.FaceNormal(f);
+                foreach (var h in geom.GetAdjacentHalfedges(f)) {
+                    var j = h.prev.vid;
+                    var e  = geom.Vector(h);
+                    var eT = cross(n, e);
+                    v += eT * (float)(scalarPotential[j] / (2 * a));
+                    v += e  * (float)(vectorPotential[j] / (2 * a));
+                }
+                var c = geom.Centroid(f);
+                var u = new float3(-c.z, 0, c.x);
+                u -= n * dot(u, n);
+                u = normalize(u);
+                v += u * 0.3f;
+                field[f] = v;
+            }
+            var omega = new double[geom.nEdges];
+            for (var i = 0; i < geom.nEdges; i++) {
+                var e = geom.Edges[i];
+                var h = geom.halfedges[e.hid];
+                var f1 = h.onBoundary ? new float3() : field[h.face];
+                var f2 = h.twin.onBoundary ? new float3() : field[h.twin.face];
+                omega[i] = dot(f1 + f2, geom.Vector(h));
+            }
+            return omega;
+        }
+
+        public static float3[] InterpolateWhitney(double[] oneForm, HeGeom geom) {
             var field = new float3[geom.nFaces];
-            for (var i = 0; i < geom.nFaces; i++)
-            {
+            for (var i = 0; i < geom.nFaces; i++) {
                 var f = geom.Faces[i];
                 var h = geom.halfedges[f.hid];
                 var pi = geom.Pos[h.vid];
--- Assets/Examples/Ribbon/VectorFieldVisualizer.cs
+++ Assets/Examples/Ribbon/VectorFieldVisualizer.cs
@@ -1,138 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
-using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
 using Unity.Mathematics;
-using ddg;
 using UnityEngine;
-using static Unity.Mathematics.math;
 using System.Linq;
 
-public class VectorFieldVisualizer : MonoMfdViewer {
-    protected HodgeDecomposition hodge;
-    protected Matrix<double> phi;
-    protected double[] omega; 
-    protected double[] dAlpha; 
-    protected double[] deltaBeta; 
-
-    protected override void Start() {
-        base.Start();
-
-        omega = GenRandomOneForm(geom);
-        hodge = new HodgeDecomposition(geom);
-        var omegaM = DenseMatrix.OfColumnMajor(omega.Length, 1, omega.Select(v => (double)v));
-        dAlpha = hodge.ComputeExactComponent(omegaM);
-        deltaBeta = hodge.ComputeCoExactComponent(omegaM);
-
-        var n = geom.nFaces;
-        var tngs = new Vector3[n * 6];
-        var mlen = 0.3f * geom.MeanEdgeLength();
-        //var omegaField = InterpolateWhitney(omega);
-        //var omegaField = InterpolateWhitney(dAlpha);
-        var omegaField = InterpolateWhitney(deltaBeta);
-        for(var i = 0; i < n; i++){
-            var face = geom.Faces[i];
-            var field = omegaField[i] * mlen;
-            var C = (float3)geom.Centroid(face);
-            var (_, N) = geom.FaceNormal(face);
-            field = ClampFieldLength(field, mlen);
-            var fc1 = C - field + N * 0.005f;
-            var fc2 = C + field + N * 0.005f;
-            var v = fc2 - fc1;
-            var vT = cross(N, v);
-            tngs[i * 6 + 0] = fc1;
-            tngs[i * 6 + 1] = fc2;
-            tngs[i * 6 + 2] = fc2;
-            tngs[i * 6 + 3] = fc2 - v * 0.2f + vT * 0.2f;
-            tngs[i * 6 + 4] = fc2;
-            tngs[i * 6 + 5] = fc2 - v * 0.2f - vT * 0.2f;
+namespace ddg {
+    public class VectorFieldVisualizer : MonoMfdViewer {
+        protected HodgeDecomposition hodge;
+    
+        protected override void Start() {
+            base.Start();
+            hodge = new HodgeDecomposition(geom);
+            var omega = TangentBundle.GenRandomOneForm(geom);
+            var omegaM = DenseMatrix.OfColumnMajor(omega.Length, 1, omega.Select(v => (double)v));
+            var dAlpha = hodge.ComputeExactComponent(omegaM);
+            //deltaBeta = hodge.ComputeCoExactComponent(omegaM);
+            tngBuffer.SetData(DrawArrow(dAlpha));
+            nrmMat.SetBuffer("_Tng", tngBuffer);
         }
-        tngBuffer.SetData(tngs);
-        nrmMat.SetBuffer("_Tng", tngBuffer);
-    }
-
-    protected override void OnRenderObject() {
-        nrmMat.SetPass(1);
-        Graphics.DrawProceduralNow(MeshTopology.Lines, geom.nFaces * 6);
-    }
-
-    protected override float GetValueOnSurface(Vert v) => (float)phi[v.vid, 0]; 
-
-    double[] GenRandomOneForm(HeGeom geom) { 
-        var n = geom.nVerts;
-        var r = max(2, (int)(n / 1000f));
-        var rho1 = DenseMatrix.Create(n, 1, 0);
-        var rho2 = DenseMatrix.Create(n, 1, 0);
-        for (var i = 0; i < r; i++) {
-            var j1 = (int)(UnityEngine.Random.value * n);
-            var j2 = (int)(UnityEngine.Random.value * n);
-            rho1[j1, 0] = UnityEngine.Random.Range(-2500f, 2500f);
-            rho2[j2, 0] = UnityEngine.Random.Range(-2500f, 2500f);
-        }
-        var scalarPotential = ScalerPoissonProblem.SolveOnSurfaceNative(geom, rho1);
-        var vectorPotential = ScalerPoissonProblem.SolveOnSurfaceNative(geom, rho2);
-
-        var field = new Dictionary<Face, float3>();
-        for (var i = 0; i < geom.nFaces; i++) {
-            var f = geom.Faces[i];
-            var v = new float3();
-            var A = geom.Area(f);
-            var (_, N) = geom.FaceNormal(f);
-            var C = geom.Centroid(f);
-            foreach(var h in geom.GetAdjacentHalfedges(f)) {
-                var j = h.prev.vid;
-                var e = geom.Vector(h);
-                var eT = cross(N, e);
-                v += eT * (float)(scalarPotential[j] / (2 * A));
-                v += e * (float)(vectorPotential[j] / (2 * A));
+    
+        Vector3[] DrawArrow(double[] omega) {
+            var n = geom.nFaces;
+            var arws = new Vector3[n * 6];
+            var mlen = 0.3f * geom.MeanEdgeLength();
+            var omegaField = TangentBundle.InterpolateWhitney(omega, geom);
+            for(var i = 0; i < n; i++){
+                var face = geom.Faces[i];
+                var field = omegaField[i] * mlen;
+                var C = (float3)geom.Centroid(face);
+                var (_, N) = geom.FaceNormal(face);
+                field = ClampFieldLength(field, mlen);
+                var fc1 = C - field + N * 0.005f;
+                var fc2 = C + field + N * 0.005f;
+                var v = fc2 - fc1;
+                var vT = math.cross(N, v);
+                arws[i * 6 + 0] = fc1;
+                arws[i * 6 + 1] = fc2;
+                arws[i * 6 + 2] = fc2;
+                arws[i * 6 + 3] = fc2 - v * 0.2f + vT * 0.2f;
+                arws[i * 6 + 4] = fc2;
+                arws[i * 6 + 5] = fc2 - v * 0.2f - vT * 0.2f;
             }
-            var u = new float3(-C.z, 0, C.x);
-            u -= N * dot(u, N);
-            u = normalize(u);
-            v += u * 0.3f;
-            field[f] = v;
+            return arws;
         }
-
-        var w = new double[geom.nEdges];
-        for (var i = 0; i < geom.nEdges; i++) {
-            var e = geom.Edges[i];
-            var h = geom.halfedges[e.hid];
-            var f1 = h.onBoundary ?      new float3() : field[h.face]; 
-            var f2 = h.twin.onBoundary ? new float3() : field[h.twin.face]; 
-            w[i] = dot(f1 + f2, geom.Vector(h));
+    
+        protected override void OnRenderObject() {
+            nrmMat.SetPass(1);
+            Graphics.DrawProceduralNow(MeshTopology.Lines, geom.nFaces * 6);
         }
-        return w;
-    }
-
-    float3[] InterpolateWhitney(double[] oneForm) {
-        var field = new float3[geom.nFaces];
-        for (var i = 0; i < geom.nFaces; i++) {
-            var f = geom.Faces[i];
-            var h = geom.halfedges[f.hid];
-            var pi = geom.Pos[h.vid];
-            var pj = geom.Pos[h.next.vid];
-            var pk = geom.Pos[h.prev.vid];
-            var eij = pj - pi;
-            var ejk = pk - pj;
-            var eki = pi - pk;
-            var cij = oneForm[h.edge.eid];
-            var cjk = oneForm[h.next.edge.eid];
-            var cki = oneForm[h.prev.edge.eid];
-            if (h.edge.hid != h.id) cij *= -1;
-            if (h.next.edge.hid != h.next.id) cjk *= -1;
-            if (h.prev.edge.hid != h.prev.id) cki *= -1;
-            var A = geom.Area(f);
-            var (_, N) = geom.FaceNormal(f);
-            var a = (eki - ejk) * (float)cij;
-            var b = (eij - eki) * (float)cjk;
-            var c = (ejk - eij) * (float)cki;
-            field[i] = cross(N, (a + b + c)) / (float)(6 * A);
+    
+        protected override float GetValueOnSurface(Vert v) => throw new System.Exception(); 
+    
+        Vector3 ClampFieldLength(Vector3 field, float len) {
+            var norm = field.magnitude;
+            return norm > len ? field * len / norm : field;
         }
-        return field;
-    }
-
-    Vector3 ClampFieldLength(Vector3 field, float length) {
-        var norm = field.magnitude;
-        return norm > length ? field * length / norm : field;
     }
 }
