f71c02dcb884bda5705d83302495fe79e3839069
komietty <komietty@gmail.com>
2022-12-02 22:00:12 +0900

    update line(wip)

--- Assets/Examples/TangentFieldViewer/TangentFieldViewer.cs
+++ Assets/Examples/TangentFieldViewer/TangentFieldViewer.cs
@@ -6,12 +6,16 @@ namespace ddg {
     public class TangentFieldViewer : TangentBundleBehaviour {
         public enum Field { Random, Exact, CoExact }
         [SerializeField] protected Field field;
+        [SerializeField] protected GameObject point;
+        [SerializeField] protected ComputeShader cs;
+        [SerializeField, Range(0, 10)] float potentialColor;
         protected bool flag = false;
         protected double[] random;
         protected double[] exact;
         protected double[] coexact;
-        protected GraphicsBuffer exactPosBuf;
-        protected GraphicsBuffer coexactPosBuf;
+        protected GraphicsBuffer scalarPotentialPos;
+        protected GraphicsBuffer vectorPotentialPos;
+        protected GraphicsBuffer distances;
 
         void OnValidate(){
             if (!flag) return;
@@ -33,21 +37,42 @@ namespace ddg {
             exact = h.ComputeExactComponent(m);
             coexact = h.ComputeCoExactComponent(m);
             UpdateTng(random);
+            
+            var t = GraphicsBuffer.Target.Structured;
+            scalarPotentialPos = new GraphicsBuffer(t, exactIds.Length,   sizeof(float) * 3);
+            vectorPotentialPos = new GraphicsBuffer(t, coexactIds.Length, sizeof(float) * 3);
+            distances          = new GraphicsBuffer(t, tngBuf.count,     sizeof(float) * 2);
+            var scalarPotPos =   exactIds.Select(i => g.Pos[i]).ToArray();
+            var vectorPotPos = coexactIds.Select(i => g.Pos[i]).ToArray();
+            foreach (var p in scalarPotPos) { var go = GameObject.Instantiate(point, p, Quaternion.identity); go.transform.SetParent(this.transform); }
+            foreach (var p in vectorPotPos) { var go = GameObject.Instantiate(point, p, Quaternion.identity); go.transform.SetParent(this.transform); }
+            scalarPotentialPos.SetData(scalarPotPos);
+            vectorPotentialPos.SetData(vectorPotPos);
 
-            exactPosBuf   = new GraphicsBuffer(GraphicsBuffer.Target.Structured, exactIds.Length,   sizeof(float) * 3);
-            coexactPosBuf = new GraphicsBuffer(GraphicsBuffer.Target.Structured, coexactIds.Length, sizeof(float) * 3);
-            exactPosBuf.SetData(exactIds.Select(i => g.Pos[i]).ToArray());
-            coexactPosBuf.SetData(coexactIds.Select(i => g.Pos[i]).ToArray());
+            var k = cs.FindKernel("SetDistance");
+            int s = Mathf.CeilToInt(tngBuf.count / 64.0f);
+            cs.SetInt("ScalarPotentialNum",   exactIds.Length);
+            cs.SetInt("VectorPotentialNum", coexactIds.Length);
+            cs.SetFloat("MeanEdgeLength", g.MeanEdgeLength());
+            cs.SetBuffer(k, "TangentArrowBuf", tngBuf);
+            cs.SetBuffer(k, "ScalarPotentialPosBuf", scalarPotentialPos);
+            cs.SetBuffer(k, "VectorPotentialPosBuf", vectorPotentialPos);
+            cs.SetBuffer(k, "Distances", distances);
+            cs.Dispatch(k, s, 1, 1);
+            tngMat.SetBuffer("_Dist", distances);
         }
 
         void Update() {
+            tngMat.SetInt("_M", (int)field);
             tngMat.SetFloat("_T", Time.time);
+            tngMat.SetFloat("_C", potentialColor);
         }
 
         protected override void OnDestroy() {
             base.OnDestroy();
-            exactPosBuf.Dispose();
-            coexactPosBuf.Dispose();
+            distances.Dispose();
+            scalarPotentialPos.Dispose();
+            vectorPotentialPos.Dispose();
         }
     }
 }
--- Assets/Examples/TangentFieldViewer/TangentFieldViewer.shader
+++ Assets/Examples/TangentFieldViewer/TangentFieldViewer.shader
@@ -13,13 +13,16 @@ Shader "ddg/TangentFieldViewer" {
             #include "UnityCG.cginc"
             #define PI 3.14159265
             StructuredBuffer<float3> _Line;
-            StructuredBuffer<float> _ExactCoExactDistances;
+            StructuredBuffer<float2> _Dist;
             float _T;
+            float _C;
+            int   _M;
 
             struct appdata { float4 vertex : POSITION; };
             struct v2f {
                 float4 vertex : SV_POSITION;
-                float2 uv: TEXCOORD0;
+                float2 uv:   TEXCOORD0;
+                float2 dist: TEXCOORD1;
             };
 
             v2f vert (uint vid: SV_VertexID, appdata val) {
@@ -28,13 +31,17 @@ Shader "ddg/TangentFieldViewer" {
                 uint iid = vid / 6;
                 uint uid = vid % 6; 
                 o.vertex = UnityObjectToClipPos(float4(p, 1));
-                o.uv = float2(uid == 0 ? 1 : 0, (float)iid);
+                o.uv   = float2(uid == 0 ? 1 : 0, (float)iid);
+                o.dist = _Dist[vid].xy;
                 return o;
             }
 
             float4 frag (v2f i) : SV_Target {
                 float  v = pow(sin(_T * 5 + i.uv.x * PI + i.uv.y) * 0.5 + 0.5, 2);
-                return float4(0.1, 0.7, 1, 1 - v * 0.8);
+                float4 o = float4(0.1, 0.5, 1, 1 - v * 0.8);
+                o += _M != 2 ? float4( 0, i.dist.x * _C, 0, 0) : (0).xxxx;
+                o += _M != 1 ? float4( 0, i.dist.y * _C, 0, 0) : (0).xxxx;
+                return o;
             }
             ENDCG
         }
