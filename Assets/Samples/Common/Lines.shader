7cdf7ea6d8a9c3ef8198f487b3f6cd864c9ebd07
komietty <komietty@gmail.com>
2022-12-14 02:00:37 +0900

    tree-cotree(wip)

--- Assets/Samples/Common/Lines.shader
+++ Assets/Samples/Common/Lines.shader
@@ -36,7 +36,7 @@ Shader "ddg/Lines" {
             float4 frag (v2f i) : SV_Target {
                 float  v = pow(sin(_T * 5 + i.uv.x * PI + i.uv.y) * 0.5 + 0.5, 2);
                 //return float4(v, 0.6 + v * 0.4, 1, v);
-                return float4(0.1, 0.7, 1, 1 - v * 0.8);
+                return float4(2, 0, 0, 1);
             }
             ENDCG
         }
--- Assets/Samples/HomologyGeneratorViewer/HomologyGeneratorViewer.cs
+++ Assets/Samples/HomologyGeneratorViewer/HomologyGeneratorViewer.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 namespace ddg {
     public class HomologyGeneratorViewer : MonoBehaviour {
+        [SerializeField] protected Material treeMat;
+        [SerializeField] protected Material cotrMat;
         protected HeGeom geom;
         protected HalfEdge[] gens;
-        protected GraphicsBuffer treeBuf;
+        protected GraphicsBuffer treeBuf, cotrBuf;
+        protected List<Vector3> tarr;
+        protected List<Vector3> carr;
             
 
         void Start() {
@@ -18,9 +23,8 @@ namespace ddg {
             h.BuildDualSpanningCotree();
             var tree = h.vertParentList;
             var cotr = h.faceParentList;
-            var tarr = new List<Vector3>(tree.Count * 2);
-            var carr = new List<Vector3>(cotr.Count * 2);
-            //treeBuf = new GraphicsBuffer();
+            tarr = new List<Vector3>(tree.Count * 2);
+            carr = new List<Vector3>(cotr.Count * 2);
 
             foreach(var item in tree) {
                 var v1 = item.Key;
@@ -32,10 +36,29 @@ namespace ddg {
             foreach(var item in cotr) {
                 var fid1 = item.Key;
                 var fid2 = item.Value;
-                Debug.Log(fid1);
-                Debug.Log(fid2);
-                Debug.Log("-----");
+                carr.Add(geom.Centroid(geom.Faces[fid1]));
+                carr.Add(geom.Centroid(geom.Faces[fid2]));
             }
+
+            treeBuf = new GraphicsBuffer(Target.Structured, tree.Count * 2, sizeof(float) * 3);
+            cotrBuf = new GraphicsBuffer(Target.Structured, cotr.Count * 2, sizeof(float) * 3);
+            treeBuf.SetData(tarr);
+            cotrBuf.SetData(carr);
+            treeMat.SetBuffer("_Line", treeBuf);
+            cotrMat.SetBuffer("_Line", cotrBuf);
+
+        }
+
+        void OnRenderObject() {
+                treeMat.SetPass(0);
+                Graphics.DrawProceduralNow(MeshTopology.Lines, tarr.Count);
+                cotrMat.SetPass(0);
+                Graphics.DrawProceduralNow(MeshTopology.Lines, carr.Count);
+        }
+
+        void OnDestroy() {
+            treeBuf.Dispose();
+            cotrBuf.Dispose();
         }
     }
 }
