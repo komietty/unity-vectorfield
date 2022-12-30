74a1a540718ad3c231a1704e281fbc00de9bb39c
komietty <komietty@gmail.com>
2022-12-31 00:15:49 +0900

    ribbon(wip)

--- Assets/Samples/Ribbon/RibbonViewer.cs
+++ Assets/Samples/Ribbon/RibbonViewer.cs
@@ -12,12 +12,11 @@ public class RibbonViewer : MonoBehaviour {
         g = new HeGeom(HeComp.Weld(GetComponent<MeshFilter>().sharedMesh), transform);
         var face = g.Faces[1];
         var curr = g.halfedges[face.hid];
-        var rate = 0.6f;
-        var tng = transform.TransformDirection(new float3(1f, -0.1f, 0f));
-        for(var i = 0; i < 2; i++) {
+        var rate = 0.8f;
+        var tng = transform.TransformDirection(new float3(1f, -0f, -0.5f));
+        for(var i = 0; i < 20; i++) {
             var (h, r) = TangentTracer.CrossHe(tng, face, curr, rate, g);
             var g1 = GameObject.Instantiate(arrow);
-            //g1.transform.position = g.Centroid(f);
             g1.transform.position = (g.Pos[curr.next.vid] - g.Pos[curr.vid]) * rate + g.Pos[curr.vid];
             g1.transform.LookAt(g1.transform.position + (Vector3)tng);
             g1.transform.localScale *= 0.02f;
@@ -28,16 +27,5 @@ public class RibbonViewer : MonoBehaviour {
             curr = h.twin;
             rate = 1f - r;
         }
-        //var ob = g.OrthonormalBasis(f);
-        //var o1 = GameObject.Instantiate(arrow);
-        //var o2 = GameObject.Instantiate(arrow);
-        //o1.transform.position = g.Centroid(f);
-        //o2.transform.position = g.Centroid(f);
-        //o1.transform.localScale *= 0.05f;
-        //o2.transform.localScale *= 0.05f;
-        //o1.name = "o1";
-        //o2.name = "o2";
-        //o1.transform.LookAt(o1.transform.position + (Vector3)ob.Item1);
-        //o2.transform.LookAt(o2.transform.position + (Vector3)ob.Item2);
     }
 }
