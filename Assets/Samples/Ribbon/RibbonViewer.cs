743642d4f26f7fc2a6a0a917a6b8bb285896d7db
komietty <komietty@gmail.com>
2022-12-31 19:47:43 +0900

    ribbon(wip)

--- Assets/Samples/Ribbon/RibbonViewer.cs
+++ Assets/Samples/Ribbon/RibbonViewer.cs
@@ -7,6 +7,9 @@ using Unity.Mathematics;
 public class RibbonViewer : TangentBundle {
     [SerializeField] protected GameObject arrow;
     float3[] tangentFields;
+    Face face;
+    HalfEdge curr;
+    float rate; 
 
     protected override void Start() {
         base.Start();
@@ -18,26 +21,56 @@ public class RibbonViewer : TangentBundle {
         tangentFields = t.GenField(t.ComputeConnections(s));
         UpdateTng(tangentFields);
         tngtMat.SetFloat("_C", 1);
-        Draw();
+
+        //tangentFields = new float3[geom.nFaces];
+        //var tmp = new float3[geom.nFaces];
+        //for (var i = 0; i < geom.nFaces; i++) {
+        //    tangentFields[i] = new float3(0, 0, -1);
+        //    tmp[i] = new float3(0, 0, 0);
+        //}
+        //UpdateTng(tmp);
+
+        //Draw();
+        face = geom.Faces[1];
+        curr = geom.halfedges[face.hid];
+        rate = 0.5f;
     }
 
-    void Draw() {
-        var face = geom.Faces[0];
-        var curr = geom.halfedges[face.hid];
-        var rate = 0.2f;
-        for(var i = 0; i < 30; i++) {
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
             var tng = tangentFields[face.fid];
             var g1 = GameObject.Instantiate(arrow);
             g1.transform.position = (geom.Pos[curr.next.vid] - geom.Pos[curr.vid]) * rate + geom.Pos[curr.vid];
             g1.transform.LookAt(g1.transform.position + (Vector3)tng);
             g1.transform.localScale *= 0.02f;
-            var (h, r) = TangentTracer.CrossHe(tng, face, curr, rate, geom);
+
             var g2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            g2.transform.position = (geom.Pos[h.next.vid] - geom.Pos[h.vid]) * r + geom.Pos[h.vid];
+            g2.transform.position = geom.Centroid(face);
             g2.transform.localScale *= 0.02f;
-            face = h.twin.face;
-            curr = h.twin;
+
+            var (hid, r) = TangentTracer.CrossHe(tng, face, curr, rate, geom);
+            r = Mathf.Min(r, 0.95f);
+            r = Mathf.Max(r, 0.05f);
+            curr = geom.halfedges[hid].twin;
+            face = curr.face;
             rate = 1f - r;
+
+        }
+    }
+
+    void Draw() {
+        for(var i = 0; i < 60; i++) {
+            var tng = tangentFields[face.fid];
+            var g1 = GameObject.Instantiate(arrow);
+            g1.transform.position = (geom.Pos[curr.next.vid] - geom.Pos[curr.vid]) * rate + geom.Pos[curr.vid];
+            g1.transform.LookAt(g1.transform.position + (Vector3)tng);
+            g1.transform.localScale *= 0.02f;
+            var (hid, r) = TangentTracer.CrossHe(tng, face, curr, rate, geom);
+            curr = geom.halfedges[hid].twin;
+            face = curr.face;
+            rate = 1f - r;
+            if(curr.onBoundary) break;
         }
     }
 }
--- Assets/VectorField/Core/VectorField/TangentTracer.cs
+++ Assets/VectorField/Core/VectorField/TangentTracer.cs
@@ -8,6 +8,8 @@ using static Unity.Mathematics.math;
 namespace ddg {
     using f2 = float2;
     using f3 = float3;
+    using d2 = double2;
+    using d3 = double3;
     public class TangentTracer {
         public static (float tray, float tline) RayLineIntersection(f2 rayBgn, f2 rayDir, f2 lineA, f2 lineB) {
             var v1 = rayBgn - lineA;
@@ -20,30 +22,33 @@ namespace ddg {
             return (tray, tline);
         }
 
-        public static f2 TangentOnFacePlane(float3 v, Face f, HeGeom g) {
+        public static d2 TangentOnFacePlane(f3 v, Face f, HeGeom g) {
                 var o = g.OrthonormalBasis(f);
-                return normalize(new f2(dot(v, o.Item1), dot(v, o.Item2)));
+                return new double2(
+                    dot((d3)v, (d3)o.Item1),
+                    dot((d3)v, (d3)o.Item2)
+                );
         }
 
-        public static (bool flag, float r) Intersect(float2 dir, float2 pos, float2 p1, float2 p2) {
+        public static (bool flag, float r) Intersect(d2 dir, d2 pos, d2 p1, d2 p2) {
             var a = pos;
-            var b = pos + dir * 1000;
+            var b = pos + dir;
             var c = p1;
             var d = p2;
-            var deno = cross(new f3(b - a, 0), new f3(d - c, 0)).z;
+            var deno = cross(new d3(b - a, 0), new d3(d - c, 0)).z;
             if (deno == 0) { return (false, 0); }
-            var s = cross(float3(c - a, 0), float3(d - c, 0)).z / deno;
-            var t = cross(float3(b - a, 0), float3(a - c, 0)).z / deno;
-            var e = 1e-3;
-            if (s < -e || 1 + e < s || t < -e || 1 + e < t) {
+            var s = cross(new d3(c - a, 0), new d3(d - c, 0)).z / deno;
+            var t = cross(new d3(b - a, 0), new d3(a - c, 0)).z / deno;
+            var e = 1e-5;
+            if (s < -e || t < -e || 1 + e < t) {
                 Debug.Log("s:" + s);
                 Debug.Log("t:" + t);
                 return (false, 0);
             }
-            return (true, t);
+            return (true, (float)t);
         }
 
-        public static (HalfEdge h, float r) CrossHe (float3 tng, Face f, HalfEdge h, float r, HeGeom g) {
+        public static (int hid, float r) CrossHe (float3 tng, Face f, HalfEdge h, float r, HeGeom g) {
             var h0 = g.halfedges[f.hid];
             var h1 = h0.next;
             var h2 = h0.prev;
@@ -53,28 +58,25 @@ namespace ddg {
             var v1 = TangentOnFacePlane(g.Pos[h1.vid] - g.Pos[h0.vid], f, g);
             var v2 = TangentOnFacePlane(g.Pos[h2.vid] - g.Pos[h0.vid], f, g);
             if (h.id == h0.id) {
-                //Debug.Log(0);
                 var pBgn = v1 * r;
                 var intersect_h1 = Intersect(dir, pBgn, v1, v2);
                 var intersect_h2 = Intersect(dir, pBgn, v2, v0);
-                if(intersect_h1.flag) return (h1, intersect_h1.r);
-                if(intersect_h2.flag) return (h2, intersect_h2.r);
+                if (intersect_h1.flag) { return (h1.id, intersect_h1.r); }
+                if (intersect_h2.flag) { return (h2.id, intersect_h2.r); }
             }
             if (h.id == h1.id) {
-                //Debug.Log(1);
                 var pBgn = v1 * (1f - r) + v2 * r;
                 var intersect_h2 = Intersect(dir, pBgn, v2, v0);
                 var intersect_h0 = Intersect(dir, pBgn, v0, v1);
-                if(intersect_h2.flag) return (h2, intersect_h2.r);
-                if(intersect_h0.flag) return (h0, intersect_h0.r);
+                if (intersect_h2.flag) { return (h2.id, intersect_h2.r); }
+                if (intersect_h0.flag) { return (h0.id, intersect_h0.r); }
             }
             if (h.id == h2.id) {
-                //Debug.Log(2);
                 var pBgn = v2 * (1f - r);
                 var intersect_h0 = Intersect(dir, pBgn, v0, v1);
                 var intersect_h1 = Intersect(dir, pBgn, v1, v2);
-                if(intersect_h0.flag) return (h0, intersect_h0.r);
-                if(intersect_h1.flag) return (h1, intersect_h1.r);
+                if (intersect_h0.flag) { return (h0.id, intersect_h0.r); }
+                if (intersect_h1.flag) { return (h1.id, intersect_h1.r); }
             }
 
             throw new System.Exception();
