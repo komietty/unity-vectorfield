6dce0ad7c7001e92092196c23199b06d599ba0bf
komietty <komietty@gmail.com>
2023-02-13 21:22:56 +0900

    update

--- Assets/Samples/HeatMethod/Vector/VectorHeatMethod.cs
+++ Assets/Samples/HeatMethod/Vector/VectorHeatMethod.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using MathNet.Numerics;
 using MathNet.Numerics.LinearAlgebra;
 using Unity.Mathematics;
@@ -6,6 +6,7 @@ using UnityEngine;
 using static Unity.Mathematics.math;
 
 namespace VectorField {
+    using RV = MathNet.Numerics.LinearAlgebra.Vector<double>;
     using RS = MathNet.Numerics.LinearAlgebra.Double.SparseMatrix;
     using DD = MathNet.Numerics.LinearAlgebra.Double.DenseMatrix;
     using CS = MathNet.Numerics.LinearAlgebra.Complex32.SparseMatrix;
@@ -17,56 +18,53 @@ namespace VectorField {
 
         public VectorHeatMethod(HeGeom geom) {
             this.geom = geom;
+            this.shm = new ScalarHeatMethod(geom);
         }
         
-        // must have right direction, but not magnitude
-        public DD ComputeVectorHeatFlow() {
-            throw new System.Exception();
-        }
-
-        public float3[] GenField(CV phi) {
-            var field = new float3[geom.nVerts];
+        CV ComputeVectorHeatFlow(CV phi) {
             var t = pow(geom.MeanEdgeLength(), 2);
             var L = Operator.ConnectionLaplace(geom);
             var F = Operator.MassComplex(geom) + L * t;
             if (!F.IsHermitian()) Debug.LogWarning("Op should be hermitian");
-            
-/*
-            var FF = Matrix<Complex32>.Build.Dense(4, 4, new Complex32(0, 0));
-            // row  
-            FF[0, 0] = new Complex32(2.16506f,0);
-            FF[0, 1] = new Complex32(0.57735f,0);
-            FF[0, 2] = new Complex32(-0.288675f,-0.5f);
-            FF[0, 3] = new Complex32(-0.288675f,0.5f);
-            // row
-            FF[1, 0] = new Complex32(0.57735f,0);
-            FF[1, 1] = new Complex32(2.16506f,0);
-            FF[1, 2] = new Complex32(-0.288675f,-0.5f);
-            FF[1, 3] = new Complex32(-0.288675f,0.5f);
-            // row  
-            FF[2, 0] = new Complex32(-0.288675f,0.5f);
-            FF[2, 1] = new Complex32(-0.288675f,0.5f);
-            FF[2, 2] = new Complex32(2.16506f,0);
-            FF[2, 3] = new Complex32(-0.288675f,-0.5f);
-            // row  
-            FF[3, 0] = new Complex32(-0.288675f,-0.5f);
-            FF[3, 1] = new Complex32(-0.288675f,-0.5f);
-            FF[3, 2] = new Complex32(-0.288675f,0.5f);
-            FF[3, 3] = new Complex32(2.16506f,0);
- */
+            var conn = F.LU().Solve(phi);
+            var vecs = new Complex32[geom.nVerts];
+            for (var i =0; i < geom.nVerts; i++) 
+                vecs[i] = conn[i] / (float)conn[i].Norm();
+            return Vector<Complex32>.Build.DenseOfArray(vecs);
+        }
 
-            var llt = F.LU();
-            var conn = llt.Solve(phi);
-            Debug.Log(L);
-            Debug.Log(conn);
+        public float3[] GenField(CV phi, List<(int vid, float value)> sources) {
+            var field = new float3[geom.nVerts];
+            var conn = ComputeVectorHeatFlow(phi);
+            var mags = ExtendScaler(sources);
+            
             foreach (var v in geom.Verts) {
                 var (e1, e2) = geom.OrthonormalBasis(v);
-                field[v.vid] = normalize(
-                    e1 * conn[v.vid].Real +
-                    e2 * conn[v.vid].Imaginary
-                    );
+                var cmp = conn[v.vid] / (float)conn[v.vid].Norm();
+                var mag = mags[v.vid];
+                var sol = cmp * (float)mag;
+                field[v.vid] = e1 * sol.Real + e2 * sol.Imaginary;
             }
             return field;
+        }
+
+        double[] ExtendScaler(List<(int vid, float value)> source) {
+            var dataRhs = new double[geom.nVerts];
+            var idctRhs = new double[geom.nVerts];
+            foreach (var s in source) {
+                dataRhs[s.vid] = s.value;
+                idctRhs[s.vid] = 1;
+            }
+            var dataSol = shm.Compute(RV.Build.DenseOfArray(dataRhs));
+            var idctSol = shm.Compute(RV.Build.DenseOfArray(idctRhs));
+            var result = new double[geom.nVerts];
+            for (var i = 0; i < geom.nVerts; i++) {
+                var data = dataSol[i];
+                var indicator = idctSol[i];
+                if (indicator != 0) result[i] = data / indicator;
+                else result[i] = 0;
+            }
+            return result;
         }
     }
 }
--- Assets/Samples/HeatMethod/Vector/VectorHeatMethodViewer.cs
+++ Assets/Samples/HeatMethod/Vector/VectorHeatMethodViewer.cs
@@ -15,16 +15,29 @@ namespace VectorField.Demo {
         void Start() {
             var filt = GetComponentInChildren<MeshFilter>();
             var mesh = HeComp.Weld(filt.sharedMesh); 
-            //var mesh = GenMesh(HeComp.Weld(filt.sharedMesh));
             filt.sharedMesh = mesh;
             geom = new HeGeom(mesh, transform);
             var vhmd = new VectorHeatMethod(geom);
             var bundle = new VectorBundle(geom);
 
             var s = new C[geom.nVerts];
-            s[0] = new C(1 / math.sqrt(2), 1 / math.sqrt(2));
+            
+            var sources = new List<(int vid, float value)>();
+            var i0 = 0;
+            var i1 = 300;
+            s[i0] = new C(1 / math.sqrt(2), 1 / math.sqrt(2));
+            s[i1] = new C(1 / math.sqrt(2), 1 / math.sqrt(2));
+            var g1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var g2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            g1.transform.position = geom.Pos[i0];
+            g2.transform.position = geom.Pos[i1];
+            g1.transform.localScale *= 0.01f;
+            g2.transform.localScale *= 0.01f;
+            GameObject.CreatePrimitive(PrimitiveType.Cube);
+            sources.Add((i0, 3));
+            sources.Add((i1, 100));
 
-            var field = vhmd.GenField(V.Build.DenseOfArray(s));
+            var field = vhmd.GenField(V.Build.DenseOfArray(s), sources);
             vertTangentArrows = bundle.GenVertTangentArrows(field);
         }
     
@@ -36,28 +49,6 @@ namespace VectorField.Demo {
 
         void OnDestroy() {
             vertTangentArrows?.Dispose();
-        }
-
-        Mesh GenMesh(Mesh mesh){
-            var m = new Mesh();
-            var vs = mesh.vertices;
-            var ts = mesh.triangles;
-            m.vertices = new [] {
-                mesh.vertices[0],
-                mesh.vertices[3],
-                mesh.vertices[2],
-                mesh.vertices[1]
-            };
-            
-            m.triangles = new []
-            {
-                0, 3, 2,
-                0, 1, 3, 
-                3, 1, 2,
-                1, 0, 2
-            };
-            m.RecalculateNormals();
-            return m;
         }
     }
 }
